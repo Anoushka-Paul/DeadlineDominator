28eb064611bbef5bfa0a00895c801381d7ba1405
Anoushka Paul <anoushkapaul06@gmail.com>
2026-01-30 18:17:25 +0530

    Changed logic to determine priority

--- backend/node-backend/src/utils/priorityEvaluator.js
+++ backend/node-backend/src/utils/priorityEvaluator.js
@@ -1,13 +1,7 @@
 export const evaluatePriority = ({
-  daysLeft,
-  credits,
-  marks,
   urgency,
 }) => {
   return (
-    (daysLeft < 5 && credits > 2) ||
-    daysLeft < 2 ||
-    marks > 15 ||
     urgency
   );
 };
